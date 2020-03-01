a6c44bcfc7301bcecdf24f1bdb2ca957b74715a2
jbzdarkid <jbzdarkid@gmail.com>
2020-03-01 13:28:26 -0800

    Ignore +sample config

---
+++ config.py
@@ -0,0 +1,5 @@
+game = 'thewitness'
+target_channel_id = 391437946844217346
+
+# Follow steps here. The bot only needs the "Send Messages" permission (2048) https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token
+token = ''
