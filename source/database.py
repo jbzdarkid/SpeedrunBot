815946cbb8904522b6ff43a63c74fa2a2ef77869
jbzdarkid <jbzdarkid@gmail.com>
2021-03-12 20:00:43 -0800

    Add get_user_by_src to support runner_runs_game

--- source/database.py
+++ source/database.py
@@ -20,6 +20,7 @@ c.execute('''CREATE TABLE IF NOT EXISTS personal_bests (
 )''')
 conn.commit()
 
+
 def add_user(twitch_username, src_id, fetch_time=datetime.now()):
   try:
     c.execute('INSERT INTO users VALUES (?, ?, ?)', (twitch_username.lower(), src_id, fetch_time.timestamp()))
@@ -27,13 +28,21 @@ def add_user(twitch_username, src_id, fetch_time=datetime.now()):
   except sqlite3.IntegrityError:
     pass
 
+
 def add_game(game_name, src_game_id, discord_channel):
   c.execute('INSERT INTO tracked_games VALUES (?, ?, ?)', (game_name, src_game_id, int(discord_channel)))
   conn.commit()
 
+
 def add_personal_best(src_id, src_game_id):
   c.execute('INSERT INTO personal_bests VALUES (?, ?)', (src_id, src_game_id)))
   conn.commit()
+
+
+def update_user_fetch_by_src(src_id):
+  c.execute('UPDATE FROM users WHERE src_id=? SET last_fetched=?', (src_id, datetime.now().timestamp()))
+  conn.commit()
+
 
 def get_user(twitch_username):
   c.execute('SELECT * FROM users WHERE twitch_username=?', (twitch_username,))
@@ -45,6 +54,18 @@ def get_user(twitch_username):
     }
   return None
 
+
+def get_user_by_src(src_id):
+  c.execute('SELECT * FROM users WHERE src_id=?', (src_id,))
+  if data := c.fetchone():
+    return {
+      'twitch_username': data[0],
+      'src_id': data[1],
+      'fetch_time': data[2],
+    }
+  return None
+
+
 def get_game(game_name):
   c.execute('SELECT * FROM tracked_games WHERE game_name LIKE ?', (game_name,))
   if data := c.fetchone():
@@ -54,6 +75,7 @@ def get_game(game_name):
       'discord_channel': data[2],
     }
   return None
+
 
 def has_personal_best(src_id, src_game_id):
   c.execute('SELECT * FROM tracked_games WHERE src_id=? AND src_game_id=?', (src_id, src_game_id))
