84ec51ed21f0429725d9a4df9c9a22f85ac17ce9
jbzdarkid <jbzdarkid@gmail.com>
2021-04-06 12:56:33 -0700

    Fix a bug with not passing the correct src_game_id
    also fix a bug with printout

--- source/generics.py
+++ source/generics.py
@@ -17,40 +17,42 @@ def track_game(game_name, discord_channel):
 
 
 def get_speedrunners_for_game2(game_names):
-  twitch_game_ids = {}
+  twitch_game_ids = []
+  src_game_ids = {}
   for game_name in game_names:
     twitch_game_id, src_game_id = database.get_game_ids(game_name)
     if not twitch_game_id:
       print(f'Failed to find game IDs for game {game_name}. Skipping.')
       continue
-    twitch_game_ids[twitch_game_id] = game_name
+    twitch_game_ids.append(twitch_game_id)
+    src_game_ids[game_name] = src_game_id
     print(f'Getting speedrunners for game {game_name} ({twitch_game_id} | {src_game_id})')
 
-  streams = twitch_apis.get_live_game_streams2(list(twitch_game_ids.keys()))
+  streams = twitch_apis.get_live_game_streams2(twitch_game_ids)
 
   print('id|username            |game name           |status')
   print('--+--------------------+--------------------+--------------------------------------')
   for i, stream in enumerate(streams):
-    print(str(i).ljust(2), end='|')
-    print(stream['user_name'].ljust(20), end='|')
-    print(stream['game_name'].ljust(20), end='|')
-
     twitch_username = stream['user_name']
+    game_name = stream['game_name']
+
+    prefix = str(i).ljust(2) + '|' + twitch_username.ljust(20) + '|' + game_name.ljust(20) + '|'
+
     src_id = src_apis.get_src_id(twitch_username)
     if src_id is None:
-      print('is not a speedrunner')
+      print(f'{prefix}is not a speedrunner')
       continue
 
-    if not src_apis.runner_runs_game(twitch_username, src_id, src_game_id):
-      print('is a speedrunner, but not of this game')
+    if not src_apis.runner_runs_game(twitch_username, src_id, src_game_ids[game_name]):
+      print(f'{prefix}is a speedrunner, but not of this game')
       continue
 
-    print('is a speedrunner, and runs this game')
+    print(f'{prefix}is a speedrunner, and runs this game')
     yield {
       'preview': stream['thumbnail_url'].format(width=320, height=180),
       'url': f'https://www.twitch.tv/{twitch_username}',
       'name': twitch_username,
       'title': stream['title'],
       'viewcount': stream['viewer_count'],
-      'game_name': stream['game_name'],
+      'game_name': game_name,
     }
