99af5c75575ddd4a9093f9654e864dd46684887b
Joseph Blackman <jbzdarkid@users.noreply.github.com>
2021-07-18 00:26:19 -0700

    Update send_error.py

--- send_error.py
+++ send_error.py
@@ -17,9 +17,12 @@ num_lines = int(sys.argv[2]) if len(sys.argv) > 2 else 10
 with Path(__file__).with_name('out.log').open('r') as f:
   last_lines = '\n'.join(f.read().split('\n')[-num_lines:])
   if len(last_lines) >= 1900: # Discord character limit, with enough space for the wrapper text.
-    last_lines = '...\n' + last_lines[-1900:]
+    last_lines = last_lines[-1900:]
+    num_lines = last_lines.count('\n')
+    last_lines = '...\n' + last_lines
   content = f'Bot crashed, last {num_lines} lines:\n```{last_lines}```'
 
+# These functions should not be moved into source/discord_apis, since there may be a typo in that file, which would cause this one to crash too.
 r = requests.post(f'{api}/users/@me/channels', json={'recipient_id': user}, headers=headers)
 if r.status_code != 200:
   print(r.text)
