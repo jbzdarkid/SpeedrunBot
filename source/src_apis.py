c4c7c6ed745b59f2036f2111b2eb4ec242add38e
Joseph Blackman <joblac@microsoft.com>
2021-07-28 09:10:49 -0700

    Handle SRC "server down" status code

--- source/src_apis.py
+++ source/src_apis.py
@@ -20,7 +20,7 @@ def get_src_id(twitch_username):
 
   # Make a network call to determine if the streamer is a speedrunner.
   j = get_json('https://www.speedrun.com/api/v1/users', params={'twitch': twitch_username})
-  if 'data' not in j or len(j['data']) == 0:
+  if len(j['data']) == 0:
     database.add_user(twitch_username, None)
     return None
   src_id = j['data'][0]['id']
