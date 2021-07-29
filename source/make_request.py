ddb7168c482c7a7e242bcec8a4639394c7572603
Joseph Blackman <joblac@microsoft.com>
2021-07-28 23:17:59 -0700

    A little bit of requests cleanup

--- source/make_request.py
+++ source/make_request.py
@@ -1,6 +1,7 @@
 import logging
 import requests
 
+
 def handle_completed_request(r):
   if r.request.method == 'POST': # Strip postdata arguments from the URL since they usually contain secrets.
     url = r.url.split('?')[0]
@@ -8,13 +9,13 @@ def handle_completed_request(r):
     url = r.url
   logging.info(f'Completed {r.request.method} request to {url} with code {r.status_code}')
 
-  if r.status_code >= 500 and r.status_code <= 599:
-    raise requests.exceptions.ConnectionError('Server Unavailable')
-  if r.status_code == 420 or r.status_code == 429: # Speedrun.com returns 420 because they can't count, I guess
-    raise requests.exceptions.ConnectionError('Server Unavailable')
+  # Probably a bit overzealous but we'll see if it's every actually a problem.
+  if (r.status_code >= 400 and r.status_code <= 599):
+    raise requests.exceptions.ConnectionError(f'{r.status_code} {r.reason.upper()}')
+
   if r.status_code >= 400 and r.status_code <= 499:
-    logging.error(f'Error while talking to {url}: {r.text}')
-    
+    logging.error(f'Client error while talking to {url}: {r.text}')
+
   if r.status_code == 204: # 204 NO CONTENT
     return ''
   else:
@@ -23,14 +24,4 @@ def handle_completed_request(r):
 
 def make_request(method, url, *args, **kwargs):
   r = requests.request(method, url, *args, **kwargs)
-  return handle_completed_request(r)
-
-
-def get_json(url, params=None, headers=None):
-  r = requests.get(url, params=params, headers=headers)
-  return handle_completed_request(r)
-
-
-def post_json(url, params=None, json=None, headers=None):
-  r = requests.post(url, params=params, json=json, headers=headers)
   return handle_completed_request(r)
