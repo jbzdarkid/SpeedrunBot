4730ac4c4f438d2bf204afae49b97b67e27e52ae
Joseph Blackman <joblac@microsoft.com>
2021-07-31 23:18:49 -0700

    Well... it's working? So, ok, time to refactor everything now

--- source/discord_websocket_apis.py
+++ source/discord_websocket_apis.py
@@ -1,77 +1,132 @@
 import asyncio
 import json
+import logging
 import random
 import websockets
 from datetime import datetime, timedelta
+from pathlib import Path
 
 class WebSocket():
-  def __init__(self, intents, on_message_recieved=None):
+  def __init__(self, on_message_recieved=None):
     self.on_message_recieved = on_message_recieved
-    self.intents = intents
+    self.intents = (1 << 9) | (1 << 10) | (1 << 12) # GUILD_MESSAGES, GUILD_MESSAGE_REACTIONS, DIRECT_MESSAGES
     self.sequence = None
+    self.connected = False
+    self.session_id = None
+
+    with Path(__file__).parent.with_name('discord_token.txt').open() as f:
+      self.token = f.read().strip()
 
 
   def run(self):
-    asyncio.get_event_loop().run_until_complete(self.connect())
-    
-
-  async def run_async(self): # TODO: I don't think I need this.
-    self.connect()
+    asyncio.get_event_loop().run_until_complete(self.run())
 
 
-  async def connect(self):
-    uri = 'wss://gateway.discord.gg/?v=9&encoding=json'
-    async with websockets.connect(uri) as websocket:
-      hello = json.loads(await websocket.recv())
-      self.heartbeat_interval = timedelta(seconds=hello['d']['heartbeat_interval'])
+  async def run(self):
+    while 1:
+      websocket = await self.connect_and_resume()
 
-
-      # https://discord.com/developers/docs/topics/gateway#heartbeating
-      await asyncio.sleep(self.heartbeat_interval.total_seconds() * random.random())
-      await self.heartbeat(websocket)
-      
-      await self.identify(websocket)
-
-      # Main loop; sleep for the heartbeat duration, while waking up for any messages.
-      while 1:
+      # Main loop: Wait for messages, interrupting for heartbeats.
+      while self.connected:
         until_next_heartbeat = self.next_heartbeat - datetime.now()
         if until_next_heartbeat.total_seconds() <= 0:
           await self.heartbeat(websocket)
           continue
 
-        msg = await asyncio.wait_for(websocket.recv(), timeout=until_next_heartbeat.total_seconds())
-        await self.handle_message(msg) # TODO: Since I neither need nor see this connection, can I change threads here? That way I don't have as much ASYNC NONSENSE to deal with.
+        msg = await self.get_message(websocket, timeout=until_next_heartbeat.total_seconds())
+        if msg:
+          await self.handle_message(msg)
+
+      await websocket.close(1001)
 
 
-  async def heartbeat(self, websocket):
-    await websocket.send(json.dumps({'op': 1, 'd': self.sequence}))
-    ack = await asyncio.wait_for(websocket.recv(), timeout=self.heartbeat_interval.total_seconds())
-    if ack and json.loads(ack)['op'] == 11:
-      self.next_heartbeat = datetime.now() + self.heartbeat_interval
-      return
-    else:
-      # "The client should then immediately terminate the connection with a non-1000 close code, reconnect, and attempt to Resume."
-      pass
+  async def connect_and_resume(self):
+    while not self.connected:
+      websocket = await websockets.connect('wss://gateway.discord.gg/?v=9&encoding=json')
+      hello = json.loads(await websocket.recv())
+      self.heartbeat_interval = timedelta(milliseconds=hello['d']['heartbeat_interval'])
+      self.connected = True # Set connected early, since both heartbeat and identify can trigger a disconnection.
 
+      # https://discord.com/developers/docs/topics/gateway#heartbeating
+      random_startup = self.heartbeat_interval.total_seconds() * random.random()
+      logging.info(f'Connecting in {random_startup} seconds')
+      await asyncio.sleep(random_startup)
+      await self.heartbeat(websocket)
 
-  async def identify(self, websocket):
-    ident = {
-      'op': 2,
-      'intents': self.intents,
-      'd': {
-        'token': 'my_token',
+      if not self.connected: # Heartbeat may cause a disconnection, per above doc.
+        await websocket.close(1001)
+        continue
+
+    # https://discord.com/developers/docs/topics/gateway#identifying
+    identify = {'op': 2, 'd': {
+        'token': self.token,
+        'intents': self.intents,
         'properties': {
-          '$os': 'windows', 
+          '$os': 'windows',
           '$browser': 'speedrunbot-jbzdarkid',
           '$device': 'speedrunbot-jbzdarkid',
         },
       }
     }
+    await websocket.send(json.dumps(identify))
+
+    # https://discord.com/developers/docs/topics/gateway#resuming
+    if self.session_id:
+      logging.info(f'Resuming {self.session_id} at {self.sequence}')
+      resume = {'op': 6, 'd': {
+          'token': self.token,
+          'session_id': self.session_id,
+          'seq': self.sequence,
+        }
+      }
+      await websocket.send(json.dumps(resume)) # Note that resuming might return Invalid Session.
+    return websocket
 
 
-  async def handle_message(msg):
+  async def heartbeat(self, websocket):
+    await websocket.send(json.dumps({'op': 1, 'd': self.sequence}))
+    ack = await self.get_message(websocket, timeout=self.heartbeat_interval.total_seconds())
+    if ack and json.loads(ack)['op'] == 11:
+      self.next_heartbeat = datetime.now() + self.heartbeat_interval
+      return
+    else:
+      # If we recieve any other message, we should disconnect, reconnect, and resume.
+      # https://discord.com/developers/docs/topics/gateway#heartbeating-example-gateway-heartbeat-ack
+      self.connected = False
+
+
+  async def get_message(self, websocket, timeout):
+    try:
+      return await asyncio.wait_for(websocket.recv(), timeout=timeout)
+    except asyncio.TimeoutError:
+      return None
+    except websockets.exceptions.ConnectionClosed as e:
+      logging.exception(f'Websocket connection closed: {e.code} {e.reason}')
+      self.disconnected = True
+
+
+  async def handle_message(self, msg):
     msg = json.loads(msg)
-    if msg['op'] == 1:
+    if msg['op'] == 0: # Dispatch
+      if msg['t'] == 'READY':
+        logging.info('Signed in as ' + msg['d']['user']['username'])
+        self.session_id = msg['d']['session_id']
+      elif msg['t'] == 'RESUMED':
+        pass
+      elif msg['t'] == 'MESSAGE_CREATE':
+        # TODO: Since I neither need to nor want to see this connection, can I post messages into a thread?
+        # That way, the main program can just be sync and slow.
+        self.on_message_recieved(msg['d'])
+      else:
+        logging.error('Not handling message type ' + msg['t'])
+
+    elif msg['op'] == 1: # Heartbeat
       await self.heartbeat()
-
-
+    elif msg['op'] == 7: # Reconnect
+      self.disconnected = True
+    elif msg['op'] == 9: # Invalid Session
+      self.disconnected = True
+      if not msg['d']: # Session is not resumable
+        self.session_id = None
+    else:
+      logging.error('Not handling message opcode ' + msg['op'])
