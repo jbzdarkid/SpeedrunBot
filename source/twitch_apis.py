34e0aa4b2756289161ca47b455a81db007df4dfb
Joseph Blackman <joblac@microsoft.com>
2022-05-28 12:02:57 -0700

    uh, okay

--- source/twitch_apis.py
+++ source/twitch_apis.py
@@ -46,11 +46,8 @@ def get_live_streams(*, game_ids=None, user_logins=None):
 
   streams = []
   while 1:
-    import traceback
-    logging.info(traceback.format_stack())
-
-
     j = make_request('GET', f'{api}/streams', params=params, get_headers=get_headers)
+    logging.info(j)
     if 'data' not in j:
       break
     streams += [stream for stream in j['data'] if stream['type'] == 'live']
