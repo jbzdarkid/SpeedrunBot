19f92fb53d4e857c9f6ffb77bd4809590193d0ba
Joseph Blackman <joblac@microsoft.com>
2022-07-10 19:49:14 -0700

    Typo

--- source/src_apis.py
+++ source/src_apis.py
@@ -159,7 +159,7 @@ def get_current_pb(new_run):
   category = new_run['category']['data']['id']
   players = set(name(player) for player in new_run['players']['data'])
   time = new_run['times']['primary_t']
-  level = new_run['level']['data']['id'] if isinstance(run['level']['data'], dict) else None
+  level = new_run['level']['data']['id'] if isinstance(new_run['level']['data'], dict) else None
 
   subcategories = get_subcategories(new_run)
   try:
