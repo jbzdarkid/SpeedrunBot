7c3d274334368a09297c012684081b7b9c14ec65
Joseph Blackman <joblac@microsoft.com>
2022-11-13 16:44:35 -0800

    Updated websocket APIs per discord's latest docs

--- source/discord_websocket_apis.py
+++ source/discord_websocket_apis.py
@@ -31,6 +31,7 @@ class WebSocket():
     self.session_id = None # Indicates whether or not we have an active session, used to resume if the connection drops.
     self.sequence = -1 # Indicates the last recieved message in the current session. Meaningless if no session is active.
     self.got_heartbeat_ack = False # Indicates whether or not we've recieved a HEARTBEAT_ACK since the last heartbeat.
+    self.resume_gateway_url = None # Custom URL from discord to use when restarting the connection
 
 
   def run(self):
@@ -40,8 +41,9 @@ class WebSocket():
   async def run_async(self):
     while 1: # This loop does not exit naturally
       # Clients are limited to 1000 IDENTIFY calls to the websocket in a 24-hour period.
+      # https://discord.com/developers/docs/topics/gateway#identifying
       # For simplicity, I just use this limit as our generic reconnection rate,
-      # so that any class of failure will not throttle the client.
+      # so that any class of failure will not throttle the client and cause me to lose my token.
       logging.info('Sleeping to avoid throttling limits')
       await asyncio.sleep(24 * 60 * 60 / 1000)
 
@@ -53,6 +55,8 @@ class WebSocket():
         logging.exception('Unable to open a websocket connection due to a socket error')
       except:
         logging.exception('Unable to open a websocket for an unknown reason')
+
+      logging.info('Successfully connected the websocket')
 
       # Message loop: Wait for messages, interrupting for heartbeats.
       while self.connected:
@@ -67,7 +71,7 @@ class WebSocket():
 
       # Message loop exited, so self.connected = False
       if websocket:
-        await websocket.close(1001)
+        await websocket.close(1005) # 1005: No error specified. Discord asks us not to use 1000 and 1001.
 
 
   def get_token(self):
@@ -77,12 +81,14 @@ class WebSocket():
 
 
   async def connect(self):
-    websocket = await websockets.connect('wss://gateway.discord.gg/?v=9&encoding=json', ping_timeout=None)
+    connection_url = self.resume_gateway_url if self.session_id else 'wss://gateway.discord.gg' # Only use the resume URL for resuming an existing session
+    websocket = await websockets.connect(connection_url + '?v=9&encoding=json', ping_timeout=None)
     hello = await self.get_message(websocket)
     if not hello:
       return
 
-    # https://discord.com/developers/docs/topics/gateway#heartbeating
+    # Upon receiving the Hello event, your app should wait heartbeat_interval * jitter where jitter is any random value between 0 and 1
+    # https://discord.com/developers/docs/topics/gateway#heartbeat-interval
     self.heartbeat_interval = json.loads(hello)['d']['heartbeat_interval'] / 1000 # Value is in millis
     random_startup = self.heartbeat_interval * random()
     logging.info(f'Connecting in {random_startup} seconds')
@@ -95,23 +101,27 @@ class WebSocket():
 
     logging.info('Successfully connected and sent initial heartbeat')
 
-    # According to discord.py, we send a RESUME instead of an IDENTIFY to restore a zombied connection.
+    # Unlike the initial connection, your app does not need to re-Identify when Resuming.
+    # https://discord.com/developers/docs/topics/gateway#preparing-to-resume
     if self.session_id:
       logging.info(f'Resuming {self.session_id} at {self.sequence}')
-
-      # https://discord.com/developers/docs/topics/gateway#resuming-example-gateway-resume
       resume = {
         'token': self.get_token(),
         'session_id': self.session_id,
         'seq': self.sequence,
       }
+      self.session_id = None # If something goes wrong during the resume, we should not try again.
+
       try:
         await self.send_message(websocket, RESUME, resume)
+        msg = await self.get_message(websocket, timeout=10)
+        logging.info(f'Post-resume message: {msg}')
+        await self.handle_message(msg, websocket)
         return websocket
       except:
         logging.exception('Failed to resume the connection')
-        self.session_id = None # If we fail to resume, we need to fall back to an IDENTIFY
 
+    # https://discord.com/developers/docs/topics/gateway#gateway-intents
     intents = 0
     if ('on_message' in self.callbacks
         or 'on_message_edit' in self.callbacks
@@ -139,7 +149,7 @@ class WebSocket():
 
   async def heartbeat(self, websocket):
     if not self.got_heartbeat_ack:
-      # https://discord.com/developers/docs/topics/gateway#heartbeating-example-gateway-heartbeat-ack
+      # https://discord.com/developers/docs/topics/gateway#heartbeat-interval-example-heartbeat-ack
       logging.error('Disconnecting because heartbeat did not get an ack since last heartbeat')
       self.connected = False
       return
@@ -154,6 +164,8 @@ class WebSocket():
       msg = await asyncio.wait_for(websocket.recv(), timeout=timeout)
       return msg
     except (asyncio.TimeoutError, asyncio.CancelledError):
+      # These are perfectly normal responses from asyncio when the timeout expires.
+      # We expect to recieve these often because messages are rarer than heartbeats.
       return None
     except websockets.exceptions.ConnectionClosedError:
       logging.exception('Disconnecting due to connection error on get')
@@ -177,8 +189,10 @@ class WebSocket():
     msg = json.loads(msg)
     if msg['op'] == DISPATCH:
       if msg['t'] == 'READY':
+        # https://discord.com/developers/docs/topics/gateway-events#ready-ready-event-fields
         self.user = msg['d']['user']
         logging.info('Signed in as ' + self.user['username'])
+        self.resume_gateway_url = msg['d']['resume_gateway_url']
         if not self.session_id:
           self.session_id = msg['d']['session_id']
           self.sequence = msg['s']
@@ -214,15 +228,13 @@ class WebSocket():
     elif msg['op'] == HEARTBEAT:
       await self.heartbeat(websocket)
     elif msg['op'] == RECONNECT:
+      # Similar to INVALID_SESSION with 'd' = true, disconnect but keep the session_id so that we can resume.
       self.connected = False
     elif msg['op'] == INVALID_SESSION:
-      # In theory, we don't have to disconnect here -- we just need to re-send our IDENTIFY.
-      # However, it's much simpler to just drop the session, and besides this is pretty rare.
+      # Disconnect so that we can resume or re-identify.
       self.connected = False
       if not msg['d']: # Session is not resumable
         self.session_id = None
-      # Short sleep, per https://discord.com/developers/docs/topics/gateway#resuming-example-gateway-resume
-      await asyncio.sleep(random() * 4 + 1)
     elif msg['op'] == HEARTBEAT_ACK:
       self.got_heartbeat_ack = True
     else:
