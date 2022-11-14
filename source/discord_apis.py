ab1a8106677e4c3bd0d1a95142400bf79cd0aa3d
Joseph Blackman <joblac@microsoft.com>
2022-11-13 17:00:04 -0800

    Remove stuff I'm not using

--- source/discord_apis.py
+++ source/discord_apis.py
@@ -22,38 +22,15 @@ def get_headers():
   return cached_headers
 
 
-"""
-# Adapted from https://github.com/Rapptz/discord.py/blob/master/discord/utils.py#L829
-_MARKDOWN_ESCAPE_SUBREGEX = '|'.join(r'\{0}(?=([\s\S]*((?<!\{0})\{0})))'.format(c) for c in ('*', '`', '_', '~', '|'))
-_MARKDOWN_ESCAPE_COMMON = r'^>(?:>>)?\s|\[.+\]\(.+\)'
-_MARKDOWN_ESCAPE_REGEX = re.compile(fr'(?P<markdown>{_MARKDOWN_ESCAPE_SUBREGEX}|{_MARKDOWN_ESCAPE_COMMON})', re.MULTILINE)
-_URL_REGEX = r'(?P<url><[^: >]+:\/[^ >]+>|(?:https?|steam):\/\/[^\s<]+[^<.,:;\"\'\]\s])'
-_MARKDOWN_STOCK_REGEX = fr'(?P<markdown>[_\\~|\*`]|{_MARKDOWN_ESCAPE_COMMON})'
-_FULL_REGEX = re.compile(f'(?:{_URL_REGEX}|{_MARKDOWN_STOCK_REGEX})', re.MULTILINE)
-
-# https://github.com/Rapptz/discord.py/blob/master/discord/utils.py#L864
 def escape_markdown(text):
-  def replacement(match):
-    groupdict = match.groupdict()
-    is_url = groupdict.get('url')
-    if is_url:
-      return is_url
-    return '\\' + groupdict['markdown']
-
-  text = _FULL_REGEX.sub(replacement, text, 0)
-  return text.replace('_', '\\_')
-"""
-
-def escape_markdown(text):
+  orig_text = text
   special_characters = '_*`>\\'
   for ch in special_characters:
     if ch in text:
       text = text.replace(ch, '\\' + ch)
+  if text != orig_text:
+    logging.info(f'Before: "{text}" After: "{orig_text}"')
   return text
-
-# You should probably only use this for testing. Bots should not be in the habit of sending DMs.
-def test_get_dm_channel(user):
-  return make_request('POST', f'{api}/users/@me/channels', json={'recipient_id': user}, get_headers=get_headers)
 
 
 def send_message(channel, content, embed=None):
